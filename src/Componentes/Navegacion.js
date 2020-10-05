bb40532b217cd83f5db2c93a76c4eb6f00f5e6d2
Jesus Seiler <63084108+seiler18@users.noreply.github.com>
2020-10-05 17:00:44 -0300

    Update Navegacion.js

--- src/Componentes/Navegacion.js
+++ src/Componentes/Navegacion.js
@@ -10,7 +10,7 @@ class Navegacion extends React.Component{
                 <NavbarBrand href="/"><h1>{this.props.titulo}</h1></NavbarBrand>
             <Nav  className="ml-auto" navbar>
                 <NavItem>
-                    <NavLink href="https://seiler18.github.io/Curriculo/" target="_blank"><b>Link al perfil de Jesus Seiler</b></NavLink>
+                    <NavLink href="https://seiler18.github.io/Curriculo/" target="_blank"><b>Curriculo Jesus Seiler</b></NavLink>
                 </NavItem>
                 <NavItem>
                     <Carro/>
