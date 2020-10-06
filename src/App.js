4b494845abadbb5760a70234be8091b010bdde39
Jesus Seiler <ichbinseiler@gmail.com>
2020-10-06 17:50:08 -0300

    agregando footer y copyright

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Container, Row } from "reactstrap";
 import Navegacion from "./Componentes/Navegacion";
 import "./App.css";
 import { listaProductos } from "./listaProductos.json";
+import FooterPage from "./Componentes/Footer";
 
 class App extends React.Component {
   constructor() {
@@ -38,7 +39,10 @@ class App extends React.Component {
           </h4>
         </b>
         <Row>{arregloComponentes}</Row>
+
+      <FooterPage/>
       </Container>
+      
     );
   }
 }
--- src/Componentes/Navegacion.js
+++ src/Componentes/Navegacion.js
@@ -7,16 +7,18 @@ class Navegacion extends React.Component{
     render(){
         return(
             <Navbar color="light" light expand="md">
-                <NavbarBrand href="/"><h1>{this.props.titulo}</h1></NavbarBrand>
-            <Nav  className="ml-auto" navbar>
+                <NavbarBrand><h1>{this.props.titulo}</h1></NavbarBrand>
+            <Nav  className="ml-6" navbar>
                 <NavItem>
                     <NavLink href="https://seiler18.github.io/Curriculo/" target="_blank"><b>Curriculo Jesus Seiler</b></NavLink>
                 </NavItem>
-                <NavItem>
-                    <Carro/>
-                </NavItem>
+               
 
             </Nav>
+            
+            <Carro/>
+               
+            
             </Navbar>
         );
     }
